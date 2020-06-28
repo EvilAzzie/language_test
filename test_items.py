cfcba917a3ac883b77b4c32c057dd09071019e50
Azzy <8tyr@inbox.ru>
2020-06-28 13:02:03 +0300

    some fixes added

--- test_items.py
+++ test_items.py
@@ -1,34 +1,11 @@
 import pytest
-import time
-import math
 from selenium import webdriver
 
-@pytest.fixture
-def browser():
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(10)
-    yield browser
-    browser.quit()
-
 class TestPage():
-
-    @pytest.mark.parametrize('url',["https://stepik.org/lesson/236895/step/1",
-                                    "https://stepik.org/lesson/236896/step/1",
-                                    "https://stepik.org/lesson/236897/step/1",
-                                    "https://stepik.org/lesson/236898/step/1",
-                                    "https://stepik.org/lesson/236899/step/1",
-                                    "https://stepik.org/lesson/236903/step/1",
-                                    "https://stepik.org/lesson/236904/step/1",
-                                    "https://stepik.org/lesson/236905/step/1"])
-    def test_answer(self,browser,url):
+    def test_answer(self,browser):
+        time.sleep(30)
+        url="http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
         browser.get(url)
-        answer = math.log(int(time.time()+2))
-        txtarea=browser.find_element_by_css_selector('textarea[class="textarea string-quiz__textarea ember-text-area ember-view"]')
-        txtarea.send_keys(str(answer))
-        button=browser.find_element_by_css_selector('button[class="submit-submission"]')
-        button.click()
-        btn=browser.find_element_by_css_selector('pre[class="smart-hints__hint"]')
-        btntxt=btn.text
-        print(btntxt)
-        assert btntxt=="Correct!"     
+        button=browser.find_element_by_css_selector('button.btn-add-to-basket')
+        assert button is not None     
         
