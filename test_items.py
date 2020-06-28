385c2393704171b1be3e4eafeda1107baa726fab
Azzy <8tyr@inbox.ru>
2020-06-28 13:28:45 +0300

    test_items.py changed

--- test_items.py
+++ test_items.py
@@ -4,9 +4,13 @@ from selenium import webdriver
 
 class TestPage():
     def test_answer(self,browser):
-        url="https://readmanga.me/beastars/vol3/17"
+        url="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(url)
-        button=browser.find_element_by_css_selector('button.btn-add-to-basket')
+        button=None
+        try:
+            button=browser.find_element_by_css_selector('button.btn-add-to-basket')
+        except:
+            pass            
         time.sleep(10)
-        assert button, "button not found"    
+        assert button is not None, "button not found"    
         
