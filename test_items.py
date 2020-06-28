56b63defd93ddb29c0b19154e0c91512f40b9f5c
Azzy <8tyr@inbox.ru>
2020-06-28 13:22:59 +0300

    test_items.py fixed

--- test_items.py
+++ test_items.py
@@ -1,11 +1,12 @@
 import pytest
+import time
 from selenium import webdriver
 
 class TestPage():
     def test_answer(self,browser):
-        time.sleep(30)
-        url="http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+        url="https://readmanga.me/beastars/vol3/17"
         browser.get(url)
         button=browser.find_element_by_css_selector('button.btn-add-to-basket')
-        assert button is not None     
+        time.sleep(10)
+        assert button, "button not found"    
         
